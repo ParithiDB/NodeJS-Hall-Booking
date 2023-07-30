4938be085b3ff31fc0f83911da31d5036e0e2c0e
ParithiDB <samu.aidindia@gmail.com>
2023-07-30 17:09:27 +0530

    env

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const express = require('express');
 const HTTP_SERVER = express();
 const bodyParser = require('body-parser')
 require('dotenv').config()
-console.log(process.env)
+
 
 HTTP_SERVER.use(bodyParser.urlencoded({ extended: false }))
 
 HTTP_SERVER.use(bodyParser.json())
 
-
-console.log(process.env)
 require('./Database/dbConfig')
 
 
 const port = 5000;
 
-
+  
 
 HTTP_SERVER.listen(port, '0.0.0.0', () => {
   console.log('SERVER STARTED', port);
